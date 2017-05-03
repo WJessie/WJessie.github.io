44ccbe90dd5899222fdc6d566306cd70c5befa0f
WJessie <wjygyf1286@126.com>
2017-05-03 17:01:04 +0800

    add
    me

---
+++ img/js/common.js
@@ -0,0 +1,328 @@
+'use script' 
+//产生随机数[n,m)
+function rnd(n,m){
+	return parseInt(Math.random()*(m-n))+n
+} 
+
+//判断数组中是否包含某个数字
+function findInArray(result,arr){
+	for( var j=0;j<arr.length;j++){
+		if(arr[j] == result){
+			return true;
+		} 
+	}
+	return false;
+}
+ 
+//变成两位数
+function toDou(n){
+	return n<10 ? '0' +n : ''+n ; 
+}
+
+//判断是不是整数
+function isInt(n){
+	return n == parseInt(n); 
+}
+
+//数字转化为字符串 
+function intToStr(n){
+	return ''+n; 
+}
+
+//通过class获取元素
+function getByClass(oParent,sClass){
+	if(oParent.getElementsByClassName){
+		return oParent.getElementsByClassName(sClass);
+	}else{//IE低版本
+		var aElt = oParent.getElementsByTagName("*");
+		var arr = [];
+		for(var i = 0;i <aElt.length;i++){
+			var arr2 = aElt[i].className.split(' ');
+			if(findInArray(sClass,arr2)){
+				arr.push(aElt[i]);
+			} 
+		}
+		return arr;
+	} 
+}
+
+//从数组中的第start个位置出开始找，找到start之后最小数的位置
+//用于选择排序
+function findMinIndex(arr,start){
+	var minIndex = start;
+	var minValue = arr[start];
+	for(var i = start;i<arr.length;i++){
+		if(arr[i] < minValue){
+			minIndex = i;
+			minValue = arr[i];
+		}
+	}
+	return minIndex;
+}
+
+//事件绑定
+function addEvent(obj,sEv,fn){
+	if(obj.addEventListener){
+		obj.addEventListener(sEv,fn,false);
+	}else{
+		obj.attachEvent('on'+sEv,fn)
+	} 
+}
+
+//事件解绑
+function removeEvent(obj,sEv,fn){
+	if(obj.removeEventListener){
+		obj.removeEventListener(sEv,fn,false);
+	}else{
+		obj.detachEvent('on'+sEv,fn)
+	} 
+}
+
+//滚轮事件
+ function addWheel(obj,fn){  
+ 	if(navigator.userAgent.toLowerCase().indexOf('firefox')!=-1){
+ 		addEvent(obj,'DOMMouseScroll',wheel)
+ 	}
+ 	else{
+ 		addEvent(obj,'mousewheel',wheel);
+ 	}
+ 	function wheel(ev){
+	 	 var oEvent = ev || event; 
+	 	 var bDown =oEvent.wheelDelta ?  (oEvent.wheelDelta < 0) : (oEvent.detail>0); 
+	 	 fn && fn(bDown);  
+	 	 oEvent.preventDefault &&oEvent.preventDefault();
+	 	 return false;
+ 	}
+ }; 
+
+//获取某个元素的属性
+ function getStyle(obj,sName){
+ 	return (obj.currentStyle||getComputedStyle(obj,false))[sName];
+ }
+ //运动框架
+function move(obj,json,options){
+ 	options=options||{};
+ 	options.duration=options.duration||700;
+ 	options.easing=options.easing||'ease-out';
+ 	
+ 	var start={};
+ 	var dis={};
+ 	for(var name in json){
+ 		start[name]=parseFloat(getStyle(obj,name));
+ 		if(isNaN(start[name])){
+ 			start[name]=1;
+ 		};
+ 		
+ 		dis[name]=json[name]-start[name];
+ 	}
+ 	var count=Math.floor(options.duration/30);
+ 	var n=0;
+ 	clearInterval(obj.timer);
+ 	obj.timer=setInterval(function (){
+ 		n++;
+ 		for(var name in json){
+ 			switch(options.easing){
+ 				case 'linear':
+ 					var cur=start[name]+dis[name]*n/count;
+ 					break;
+ 				case 'ease-in':
+ 					var cur=start[name]+dis[name]*Math.pow(n/count,3);
+ 					break;
+ 				case 'ease-out':
+ 					var cur=start[name]+dis[name]*(1-Math.pow(1-n/count,3));
+ 					break;
+ 			}
+ 			if(name=='opacity'){
+ 				obj.style.opacity=cur;
+ 				obj.style.filter='alpha(opacity:'+cur*100+')';
+ 			}else{
+ 				obj.style[name]=cur+'px';
+ 			}
+ 		}
+ 		if(n==count){
+ 			clearInterval(obj.timer);
+ 			options.complete&&options.complete();
+ 		}
+ 	},30);
+ }
+//弹性运动
+ ;(function (){
+ 	var start=0;
+ 	var iSpeed=20;
+ 	window.startMove=function (obj,name,iTarget){
+ 		clearInterval(obj.timer);
+ 		obj.timer=setInterval(function (){
+ 			iSpeed+=(iTarget-start)/5;
+ 			iSpeed*=0.8;
+ 			start+=iSpeed;
+ 			obj.style[name]=start+'px';
+ 			
+ 			if(Math.round(iSpeed)==0&&Math.round(start)==iTarget){
+ 				clearInterval(obj.timer);
+ 				obj.style[name]=iTarget+'px';
+ 			}
+ 		},16);
+ 	};
+ })();
+function getDir(obj,ev){
+ 	var x = obj.offsetLeft + obj.offsetWidth/2 - ev.clientX;
+ 	var y = obj.offsetTop + obj.offsetHeight/2 - ev.clientY;
+ 	return Math.round((Math.atan2(y,x)*180/Math.PI + 180)/90)%4;
+ }
+ //穿墙效果 宽高定死
+function through(obj){
+ 	var oLi = obj.children[0];
+ 	obj.onmouseenter = function(ev){
+ 		var oEvent = ev || event;
+ 		var dir = getDir(obj,oEvent); 
+ 		switch(dir){
+ 			case 0:
+ 				oLi.style.left = '200px';
+ 				oLi.style.top = 0;
+ 				break;
+ 			case 1:
+ 				oLi.style.left = 0 ;
+ 				oLi.style.top = '160px';
+ 				break;
+ 			case 2:
+ 				oLi.style.left = '-200px';
+ 				oLi.style.top = 0;
+ 				break;
+ 			case 3:
+ 				oLi.style.left = 0;
+ 				oLi.style.top = '-160px';
+ 				break;
+
+ 		}
+ 		move(oLi,{left:0,top:0});
+ 	}
+ 	obj.onmouseleave = function(ev){
+ 		var oEvent = ev || event;
+ 		var dir = getDir(obj,oEvent);
+ 		switch(dir){
+ 			case 0: 
+ 				move(oLi,{left:200,top:0})
+ 				break;
+ 			case 1: 
+ 				move(oLi,{left:0,top:160})
+ 				break;
+ 			case 2: 
+ 				move(oLi,{left:-200,top:0})
+ 				break;
+ 			case 3: 
+ 				move(oLi,{left:0,top:-160})
+ 				break; 
+ 		} 
+ 	} 
+ }
+ 'use strict'
+var Tween={Linear:function(t,b,c,d){return c*t/d+b},Quad:{easeIn:function(t,b,c,d){return c*(t/=d)*t+b},easeOut:function(t,b,c,d){return -c*(t/=d)*(t-2)+b},easeInOut:function(t,b,c,d){if((t/=d/2)<1){return c/2*t*t+b}return -c/2*((--t)*(t-2)-1)+b}},Cubic:{easeIn:function(t,b,c,d){return c*(t/=d)*t*t+b},easeOut:function(t,b,c,d){return c*((t=t/d-1)*t*t+1)+b},easeInOut:function(t,b,c,d){if((t/=d/2)<1){return c/2*t*t*t+b}return c/2*((t-=2)*t*t+2)+b}},Quart:{easeIn:function(t,b,c,d){return c*(t/=d)*t*t*t+b},easeOut:function(t,b,c,d){return -c*((t=t/d-1)*t*t*t-1)+b},easeInOut:function(t,b,c,d){if((t/=d/2)<1){return c/2*t*t*t*t+b}return -c/2*((t-=2)*t*t*t-2)+b}},Quint:{easeIn:function(t,b,c,d){return c*(t/=d)*t*t*t*t+b},easeOut:function(t,b,c,d){return c*((t=t/d-1)*t*t*t*t+1)+b},easeInOut:function(t,b,c,d){if((t/=d/2)<1){return c/2*t*t*t*t*t+b}return c/2*((t-=2)*t*t*t*t+2)+b}},Sine:{easeIn:function(t,b,c,d){return -c*Math.cos(t/d*(Math.PI/2))+c+b},easeOut:function(t,b,c,d){return c*Math.sin(t/d*(Math.PI/2))+b},easeInOut:function(t,b,c,d){return -c/2*(Math.cos(Math.PI*t/d)-1)+b}},Expo:{easeIn:function(t,b,c,d){return(t==0)?b:c*Math.pow(2,10*(t/d-1))+b},easeOut:function(t,b,c,d){return(t==d)?b+c:c*(-Math.pow(2,-10*t/d)+1)+b},easeInOut:function(t,b,c,d){if(t==0){return b}if(t==d){return b+c}if((t/=d/2)<1){return c/2*Math.pow(2,10*(t-1))+b}return c/2*(-Math.pow(2,-10*--t)+2)+b}},Circ:{easeIn:function(t,b,c,d){return -c*(Math.sqrt(1-(t/=d)*t)-1)+b},easeOut:function(t,b,c,d){return c*Math.sqrt(1-(t=t/d-1)*t)+b},easeInOut:function(t,b,c,d){if((t/=d/2)<1){return -c/2*(Math.sqrt(1-t*t)-1)+b}return c/2*(Math.sqrt(1-(t-=2)*t)+1)+b}},Elastic:{easeIn:function(t,b,c,d,a,p){if(t==0){return b}if((t/=d)==1){return b+c}if(!p){p=d*0.3}if(!a||a<Math.abs(c)){a=c;var s=p/4}else{var s=p/(2*Math.PI)*Math.asin(c/a)}return -(a*Math.pow(2,10*(t-=1))*Math.sin((t*d-s)*(2*Math.PI)/p))+b},easeOut:function(t,b,c,d,a,p){if(t==0){return b}if((t/=d)==1){return b+c}if(!p){p=d*0.3}if(!a||a<Math.abs(c)){a=c;var s=p/4}else{var s=p/(2*Math.PI)*Math.asin(c/a)}return(a*Math.pow(2,-10*t)*Math.sin((t*d-s)*(2*Math.PI)/p)+c+b)},easeInOut:function(t,b,c,d,a,p){if(t==0){return b}if((t/=d/2)==2){return b+c}if(!p){p=d*(0.3*1.5)}if(!a||a<Math.abs(c)){a=c;var s=p/4}else{var s=p/(2*Math.PI)*Math.asin(c/a)}if(t<1){return -0.5*(a*Math.pow(2,10*(t-=1))*Math.sin((t*d-s)*(2*Math.PI)/p))+b}return a*Math.pow(2,-10*(t-=1))*Math.sin((t*d-s)*(2*Math.PI)/p)*0.5+c+b}},Back:{easeIn:function(t,b,c,d,s){if(s==undefined){s=1.70158}return c*(t/=d)*t*((s+1)*t-s)+b},easeOut:function(t,b,c,d,s){if(s==undefined){s=1.70158}return c*((t=t/d-1)*t*((s+1)*t+s)+1)+b},easeInOut:function(t,b,c,d,s){if(s==undefined){s=1.70158}if((t/=d/2)<1){return c/2*(t*t*(((s*=(1.525))+1)*t-s))+b}return c/2*((t-=2)*t*(((s*=(1.525))+1)*t+s)+2)+b}},Bounce:{easeIn:function(t,b,c,d){return c-Tween.Bounce.easeOut(d-t,0,c,d)+b},easeOut:function(t,b,c,d){if((t/=d)<(1/2.75)){return c*(7.5625*t*t)+b}else{if(t<(2/2.75)){return c*(7.5625*(t-=(1.5/2.75))*t+0.75)+b}else{if(t<(2.5/2.75)){return c*(7.5625*(t-=(2.25/2.75))*t+0.9375)+b}else{return c*(7.5625*(t-=(2.625/2.75))*t+0.984375)+b}}}},easeInOut:function(t,b,c,d){if(t<d/2){return Tween.Bounce.easeIn(t*2,0,c,d)*0.5+b}else{return Tween.Bounce.easeOut(t*2-d,0,c,d)*0.5+c*0.5+b}}}};
+
+
+//t  当前时间 		n*duration/count
+//b  初始值 			start
+//c  现在位置 		总距离
+//d  总时间 			duration
+//var cur=fx(t,b,c,d) 
+ 
+function doMove(obj,json,options){
+	options=options||{};
+	options.duration=options.duration||700;
+	options.easing=options.easing||Tween.Bounce.easeOut;
+	var start={};
+	var dis={};	
+	
+	for(var name in json){
+		start[name]=parseFloat(getStyle(obj,name));
+		//opacity加默认值
+		if(isNaN(start[name])){
+			start[name]=1;
+		}
+		//alert(json[name]);
+		dis[name]=parseFloat(json[name])-start[name];
+	}
+	
+	var count=Math.floor(options.duration/16);
+	
+	var n=0;
+	clearInterval(obj.timer);
+	obj.timer=setInterval(function (){
+		n++;
+		for(var name in json){
+			var cur=options.easing(n*options.duration/count,start[name],dis[name],options.duration);
+			if(name=='opacity'){
+				obj.style.opacity=cur;
+				obj.style.filter='alpha(opacity:'+cur*100+')';
+			}else{
+				obj.style[name]=cur+'px';
+			}
+		}
+		if(n==count){
+			clearInterval(obj.timer);
+			options.complete&&options.complete();
+		}
+	},16);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ move/js/main.js
@@ -0,0 +1,402 @@
+'use strict'
+
+ //设置高度
+;(function(){
+	$('#box').height($(window).height());
+	$('.box').height($(window).height());
+	$("#ul1").height($('#ul1 .box').length * $('#ul1 .box').height());
+	$('.allSkill').height($(window).height());
+	$('.allSkill').css('top',-$(window).height()+'px');
+	$(window).resize(function(){
+		$('#box').height($(window).height());
+		$('.box').height($(window).height());
+		$('.allSkill').height($(window).height());
+		$('.allSkill').css('top',-$(window).height()+'px');
+		var left = $('#pos')[0].offsetLeft;
+		var index = left/100;
+		var t = - index * $(window).height();
+		$('#ul1').css('top',t);
+		$('.allSkill').css('top',0);
+		if(index!= 4){
+			$('.allSkill').css('top',-$(window).height()+'px');
+			$(".oneLine").each(function(i,item){
+				$(item).css('height',0);
+			})
+		}
+
+
+	})
+})();
+
+// fnWheel();
+//滚轮滚动时
+function changeNav(t){
+	var index = -t/$(window).height();
+	$('#pos').css('background-color',$('#box'+index).css('background-color'));
+	startMove($('#pos')[0],'left',$('#navUl li:eq('+index+')')[0].offsetLeft);
+}
+//滚轮滚动时，设置ul的top值
+function changeMain(t){
+	$('#box4').css('background','#ccf');
+	if(t/$(window).height() != -4){
+		$('.allSkill').css('top',-$(window).height()+'px');
+		$(".oneLine").each(function(i,item){
+			$(item).css('height',0);
+		})
+	}
+	move($('#ul1')[0],{
+		top:t
+	},{
+		easing:'ease-out',
+		duration:600,
+		complete:function(){
+			if(t/$(window).height() == -4){
+				$('.allSkill').css('top','0px');
+				ballFall();
+			}
+			changeNav(t)
+			bOk =  true;
+		}
+	})
+}
+//滚动滚轮
+var bOk = true;
+
+function fnWheel(){
+ 	addWheel(document,function(bDown){
+ 		if(bOk){
+ 			bOk = false;
+ 			if(bDown){
+ 				var t = $('#ul1').position().top - $(window).height();
+ 				if(t <= -($('#ul1').height() - $(window).height())){
+ 					t =  -($('#ul1').height() - $(window).height());
+ 				}
+ 				changeMain(t);
+ 			}else{
+ 				var t = $('#ul1').position().top + $(window).height();
+ 				if(t >= 0){
+ 					t =  0;
+ 				}
+ 				changeMain(t);
+ 			}
+ 		}
+ 	})
+ }
+ fnWheel();
+ $('#box0').css({background:'#87CEEB'});
+ $('#nav').show();
+$('#Introduce').show();
+// 开始加载动画
+// ;(function(){
+// 	setTimeout(function(){
+// 		$('#logoBox img').animate({
+// 			opacity:0
+// 		},function(){
+// 			$('#logoBox').animate({
+// 				width:'2000px',
+// 				height:'2000px',
+// 				left:-(2000- $(window).width())/2,
+// 				top:-(2000- $(window).height())/2,
+// 			},3000,function(){
+// 				$('#logoBox').css({
+// 					'left':0,
+// 					'top':0,
+// 					'border-radius':0,
+// 					'width':$(window).width()+'px',
+// 					'height':$(window).height()+'px',
+// 					'margin-left':0,
+// 					'margin-top':0
+// 				});
+// 				$('#box0').css({background:'#22c3aa'});
+// 				$('#nav').show();
+// 				$('#Introduce').show();
+// 				$('#logoBox').animate({
+// 					opacity:0
+// 				},500,function(){
+// 					$('#logoBox').remove();
+// 				})
+// 				//动画加载完成，加载滚轮事件
+// 				fnWheel();
+// 			})
+// 		});
+// 	},1000);
+// })();
+//导航菜单
+function navMouseOut(){
+	var index = -$('#ul1').offset().top/$(window).height();
+	var left = $('#navUl li').eq(index)[0].offsetLeft;
+	startMove($('#pos')[0],'left',left);
+}
+;(function(){
+	var left=0;
+	$('#navUl li').length = $('#navUl li').length - 1;
+	$('#navUl li').each(function(i,item){
+		$(this).mouseover(function(){
+			startMove($('#pos')[0],'left',this.offsetLeft);
+		})
+		$(this).bind('mouseout',navMouseOut);
+		$(this).click(function(){
+			$(this).unbind('mouseout',navMouseOut)
+			left=this.offsetLeft;
+			startMove($('#pos')[0],'left',left);
+			changeMain(-i*$(window).height());
+		})
+	})
+})();
+
+//角转弧
+function d2a(n){
+	return n*Math.PI/180;
+}
+//弧转角
+function a2d(n){
+	return n*180/Math.PI;
+}
+
+//穿墙效果
+;(function(){
+	$('#moveBox ul').each(function(i,item){
+		through(this);
+	})
+})();
+
+;(function(){
+	var arr = [{
+		height:320,
+		lineColor:'#f66',
+		ballColor:'#f66',
+		text:'XHTML/CSS',
+		txt:'熟练掌握XHTML/CSS，能够相互配合完成兼容各种浏览器的页面'
+	},{
+		height:300,
+		lineColor:'#339',
+		ballColor:'#339',
+		text:'HTML5/CSS3',
+		txt:'熟练掌握HTML5/CSS3,能够实现各种CSS3动画，和各种移动端布局'
+	},{
+		height:320,
+		lineColor:'#069',
+		ballColor:'#069',
+		text:'JS',
+		txt:'掌握JavaScript，对JavaScript的各大对象有深入了解，熟练掌握JQuery、Zepto库、熟悉面向对象、模块化开发'
+	},{
+		height:200,
+		lineColor:'#06c',
+		ballColor:'#06c',
+		text:'node.js',
+		txt:'熟悉node.js，能够用node抓取网页数据，node.js搭建个人博客'
+	},{
+		height:280,
+		lineColor:'#f90',
+		ballColor:'#f90',
+		text:'ajax',
+		txt:'熟练掌握AJAX、JSONP,独立完成过微博发表,许愿墙等小项目,获取百度搜索数据等交互案例'
+	},{
+		height:250,
+		lineColor:'#f60',
+		ballColor:'#f60',
+		text:'vue.js',
+		txt:'熟悉vue.js，使用vue.js完成外卖app开发，了解AngularJs、react.js'
+	},{
+		height:200,
+		lineColor:'#09c',
+		ballColor:'#09c',
+		text:'grunt',
+		txt:'熟悉webpack配置，用其打包外卖app，了解grunt、gulp等前端自动化构建工具'
+	},{
+		height:50,
+		lineColor:'#f66',
+		ballColor:'#f66',
+		text:'php',
+		txt:'了解php，了解php的基本语法和简单语句'
+	},{
+		height:80,
+		lineColor:'#fc3',
+		ballColor:'#fc3',
+		text:'java',
+		txt:'熟悉java，学校时期完成过超市管理系统'
+	},{
+		height:120,
+		lineColor:'#f96',
+		ballColor:'#f96',
+		text:'c',
+		txt:'熟悉c语言，学校时期完成各种管理系统'
+	} ]
+	$(arr).each(function(){
+		$('<div class="oneSkill"><div class="oneLine"></div><div class="oneBall"></div><div class="oneTxt"></div></div>')
+		.appendTo($('.skillBox').get(0));
+	})
+	$('<div id="showTxt"><span></span><div>').appendTo($('.skillBox').get(0));
+	$('.oneTxt').each(function(i,item){//设置描述文字
+		$(item).html(arr[i].txt);
+	})
+	//移入小球
+	$('.oneBall').mouseover(function(){
+
+		$('.oneBall').each(function(i,item){  //恢复小球原来的颜色
+			$(item).css({
+				'background':arr[i].ballColor,
+				color:'#fff'
+			});
+			$(item).html(arr[i].text);
+		})
+		$(".oneLine").each(function(i,item){ //恢复线的颜色
+			$(item).css('background',arr[i].lineColor);
+		})
+		var _this = this;
+		$('#showTxt span').html("");
+		doMove($('#showTxt span')[0],{
+			top:60
+		},{
+			complete:function(){
+				$('#showTxt span').html($('.oneTxt').eq($(_this).index('.oneBall')).html()); //改变显示文字
+				doMove($('#showTxt span')[0],{
+					top:30
+				})
+			}
+		})
+		$('#box4').css({
+			opacity:0.8,
+			'backgroundColor':$(this).css('backgroundColor')
+		});
+		$('#box4').animate({
+			opacity:1
+		})
+	 	$(this).css({
+	 		background:'#fff',
+	 		color:$('#box4').css('backgroundColor')
+	 	})
+	 	$('.oneLine').eq($(this).index('.oneBall')).css({
+	 		background:'#fff'
+	 	})
+	})
+	//小球下落
+	window.ballFall = function (){
+		//9c3
+		$('.oneBall').each(function(i,item){
+			$(item).css({
+				'background':arr[i].ballColor,
+				'color':'#fff'
+			});
+			$(item).html(arr[i].text);
+		})
+		$('.oneBall').animate({
+			'opacity':1
+		})
+		$(".oneLine").each(function(i,item){
+			$(item).css('background',arr[i].lineColor);
+			doMove(item,{
+				'height':arr[i].height
+			},{
+				'duration':3000
+			})
+		})
+	}
+
+})();
+//折纸效果
+;(function(){
+	var iDelay=200;
+	var oTimer=null;
+	var i=0;
+	var bOff=true;
+	setTimeout(function(){
+		oTimer=setInterval(function(){
+			$('.perData div').eq(i).removeClass('hide');
+			$('.perData div').eq(i).addClass('show');
+			i++;
+			if(i==$('.perData div').length)
+			{
+				clearInterval(oTimer);
+				oTimer=null;
+				bOff=false;
+			}
+		},iDelay);
+	},5000)
+
+	$('.perData h2').click(function(){
+		if(oTimer){
+			return;
+		}
+		if(bOff){
+			i=0;
+			oTimer=setInterval(function(){
+				$('.perData div').eq(i).removeClass('hide');
+				$('.perData div').eq(i).addClass('show');
+				i++;
+				if(i==$('.perData div').length)
+				{
+					clearInterval(oTimer);
+					oTimer=null;
+					bOff=false;
+				}
+			},iDelay);
+		}
+		else{
+			i=$('.perData div').length-1;
+			oTimer=setInterval(function(){
+				$('.perData div').eq(i).removeClass('show');
+				$('.perData div').eq(i).addClass('hide');
+				i--;
+				if(i<0)
+				{
+					clearInterval(oTimer);
+					bOff=true;
+					oTimer=null;
+				}
+			},iDelay);
+		}
+	})
+})();
+
+//3d幻灯片效果
+;(function(){
+	var oPicList=document.getElementById("picList");
+	var oCss=document.getElementById("css");
+	var aeBtns=document.getElementById("eBtns").getElementsByTagName("li");
+	var aLi=null;
+	var sLi="";
+	var sCss="";
+	var iLiw=40;
+	var iZindex=0;
+	var iNow=0;
+	var iLength=oPicList.clientWidth/iLiw;
+	var arr = ["effect/翻页.html",
+				"effect/分块翻转.html",
+				"effect/3d旋转.html",
+				"effect/爆炸.html"
+	]
+	for(var i=0;i<iLength;i++)
+	{
+	    i>iLength/2?iZindex--:iZindex++;
+	    sLi+='<li><a href="'+arr[0]+'"></a><a href="'+arr[1]+'"></a><a href="'+arr[2]+'"></a><a href="'+arr[3]+'"></a><span></span><span></span></li>';
+	    sCss+="#picList li:nth-of-type("+(i+1)+") a{ background-position:-"+i*iLiw+"px 0;}";
+	    sCss+="#picList li:nth-of-type("+(i+1)+"){z-index:"+iZindex+"}";
+
+
+	}
+	oPicList.innerHTML=sLi;
+	oCss.innerHTML+=sCss;
+	aLi=oPicList.children;
+	for(var i=0;i<aeBtns.length;i++)
+	{
+	    (function(a){
+	        aeBtns[a].onclick=function()
+	        {
+	            for(var i=0;i<aLi.length;i++)
+	            {
+	                aLi[i].style.transition="0.5s "+i*50+"ms";
+	                aLi[i].style.WebkitTransform="rotateX(-"+a*90+"deg)";
+	            }
+	            this.className="active";
+	            aeBtns[iNow].className="";
+	            iNow=a;
+	        };
+	    })(i)
+	}
+
+})();
+
+
+
+
